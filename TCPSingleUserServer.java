ad560ab63eb3c908815681f28c1d1f4495dd948c
Felix <850369138@qq.com>
2017-12-08 15:20:06 +0800

    Update TCPSingleUserServer.java

--- TCPSingleUserServer.java
+++ TCPSingleUserServer.java
@@ -18,9 +18,15 @@ class ServerThread extends Thread {
 	public void run(){
 		try{
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String str = in.readLine() + " entered the room.";
+			System.out.println(str);
+			for(Socket socket1:list){
+				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket1.getOutputStream())), true);
+				out.println(str);
+			}
 			while(true) {
-				String str = in.readLine();
-				if (str.split(":")[2].equals(" BYE"))
+				str = in.readLine();
+				if (str.split(":")[1].equals(" BYE"))
 					break;
 				System.out.println(str);
 				for(Socket socket1:list){
@@ -29,7 +35,7 @@ class ServerThread extends Thread {
 				}
 			}
 			list.remove(socket);
-			System.out.println(socket.getRemoteSocketAddress().toString().split("/")[1] + " leave the room.");
+			System.out.println(socket.getRemoteSocketAddress().toString().split("/")[1] + " left the room.");
 			socket.close();
 		}
 		catch (Exception e){
@@ -49,7 +55,7 @@ public class TCPSingleUserServer {
 			System.out.println("Started: " + server);
 			while(true) {
 				Socket socket = server.accept();
-				System.out.println(socket.getRemoteSocketAddress().toString().split("/")[1] + " enter the room.");
+				//System.out.println(socket.getRemoteSocketAddress().toString().split("/")[1] + " entered the room.");
 				new ServerThread(socket).start();
 			}
 		}
