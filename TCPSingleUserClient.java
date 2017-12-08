c1be0cd1d7fc31a76608021b98f31099950d9d7f
Felix <850369138@qq.com>
2017-12-08 15:20:37 +0800

    Update TCPSingleUserClient.java

--- TCPSingleUserClient.java
+++ TCPSingleUserClient.java
@@ -4,56 +4,49 @@ import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 class Listener extends Thread {
-	Socket socket;
-	BufferedReader in;
-	CountDownLatch latch;
-	public Listener(Socket socket, CountDownLatch _latch) {
-		super(socket.getLocalSocketAddress().toString().split("/")[1]);
+	private Socket socket;
+	private BufferedReader in;
+
+	public Listener(Socket socket, String nickname) {
+		super(nickname);
 		this.socket = socket;
-		
-		latch = _latch;
 	}
 
 	public void run() {
-		try{
+		try {
 			in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			while (true) {
+			while (socket.isClosed() == false && socket.isConnected() == true) {
 				String str = in.readLine();
-				if (str.split(":")[2].equals("BYE"))
+				if (str.split(":")[1].equals(" BYE"))
 					break;
 				System.out.println(str);
 			}
 			in.close();
 			socket.close();
 		}
-		catch(Exception e)
-		{
+		catch(Exception e) {
 
 		}
-		latch.countDown();
 	}
 
 }
 
 class Speaker extends Thread {
-	Socket socket;
-	PrintWriter out;
-	CountDownLatch latch;
-	public Speaker(Socket socket, CountDownLatch _latch) {
-		super(socket.getLocalSocketAddress().toString().split("/")[1]);
+	private Socket socket;
+	private PrintWriter out;
+
+	public Speaker(Socket socket, String nickname) {
+		super(nickname);
 		this.socket = socket;
-		latch = _latch;
 	}
 
 	public void run() {
 		try{
 			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);			
+			out.println(this.getName()+"["+this.socket.getLocalSocketAddress().toString().split("/")[1]+"]");			
 			Scanner sc = new Scanner(System.in);
-			while(true) {
+			while(socket.isClosed() == false && socket.isConnected() == true) {
 				String s = sc.nextLine();
-				//out.println("Sending Line " + i + " ....");
-				//String str = in.readLine();
-				//System.out.println(in.readLine());
 				out.println(this.getName() + ": " + s);
 				if(s.equals("BYE")) break;
 			}
@@ -61,36 +54,33 @@ class Speaker extends Thread {
 			out.close();
 			socket.close();
 		}
-		catch(Exception e)
-		{
+		catch(Exception e) {
 
 		}
-		latch.countDown();
 	}
 }
 
 public class TCPSingleUserClient {
 	public static final int PORT = 8080;
-	public static void main(String[] args) throws IOException {
 
-		InetAddress addr = InetAddress.getByName("172.22.27.186");
+	public static void main(String[] args) throws IOException {
+		String nickname;
+		System.out.print("Please enter your Nickname: ");
+		nickname = new BufferedReader(new InputStreamReader(System.in)).readLine();
+		InetAddress addr = InetAddress.getByName("202.192.34.44");
 
 		System.out.println("addr = " + addr);
 		Socket socket = new Socket(addr, PORT);
 		try {
 			System.out.println("socket = " + socket);
 			System.out.println("Successful connected!");
-			CountDownLatch latch = new CountDownLatch(2);
-			Listener L = new Listener(socket, latch);
+			Listener L = new Listener(socket, nickname);
 			L.start();
-			Speaker S = new Speaker(socket, latch);
+			Speaker S = new Speaker(socket, nickname);
 			S.start();
 			while(socket.isClosed() == false && socket.isConnected() == true) ;
-			//latch.await();
-			
 		} 
-		catch(Exception e)
-		{
+		catch(Exception e) {
 
 		}
 		finally {
